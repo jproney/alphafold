19b01fe69ea40cfa1b619bdd5255150f676980dc
Petey <pdotjpr@gmail.com>
2021-09-02 15:56:50 -0400

    quick charge

--- alphafold/model/modules.py
+++ alphafold/model/modules.py
@@ -359,7 +359,7 @@ class AlphaFold(hk.Module):
               [num_residues, num_residues, emb_config.pair_channel]),
           'prev_predicted_lddt': jnp.zeros([num_residues, 50]),
       }
-      if all_cycles:
+      if all_reps:
          prev['prev_per_layer_pair'] = jnp.zeros([emb_config.evoformer_num_block, 1, num_residues, emb_config.msa_channel])  
          prev['prev_per_layer_msa'] = jnp.zeros([emb_config.evoformer_num_block, num_residues, num_residues, emb_config.pair_channel])    
 
